7321f6fff21e8f6c49593be32d5c3a698537114e
Lucas Martins Rangel <lucas.mrangel@sempreceub.com>
2021-06-20 20:25:56 -0300

    pagina de adicionar produto

---
+++ src/pages/estoquista/addFornecedor.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import '../../styles/pages/login.scss';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, FormGroup, Label, Input, Card, CardBody, CardHeader, Button } from 'reactstrap';
+
+interface IAddFornecedorProps extends StateProps, DispatchProps {}
+
+class AddFornecedor extends React.Component<IAddFornecedorProps> {
+  render() {
+    return (
+      <div className="d-flex h-150 align-items-center justify-content-center">
+        <Card className="w-50 shadow-lg">
+          <CardHeader className="bg-dark text-white">Adicionar fornecedor</CardHeader>
+          <CardBody>
+            <Form>
+              <FormGroup row>
+                <Label for="exampleEmail">Email de contato</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="exemplo@gmail.com" />
+              </FormGroup>
+              <br></br>
+              <FormGroup>
+                <Label>Nome do responsável</Label>
+                <Input placeholder="Digite aqui..."></Input>
+              </FormGroup>
+              <br></br>
+              <FormGroup>
+                <Label>CPF/CNPJ</Label>
+                <Input placeholder="Digite aqui..."></Input>
+              </FormGroup>
+              <br></br>
+              <FormGroup>
+                <Label>Nome fantasia ou razão social</Label>
+                <Input placeholder="Digite aqui..."></Input>
+              </FormGroup>
+              <br></br>
+              <FormGroup>
+                <Label>Endereço</Label>
+                <Input placeholder="Digite aqui..."></Input>
+              </FormGroup>
+              <br></br>
+              <FormGroup>
+                <Label>Telefone responsável</Label>
+                <Input placeholder="xxxxx-xxxx"></Input>
+              </FormGroup>
+            </Form>
+            <Link to="/user/fornecedor">
+              <Button className="mb-4 float-right" color="success">
+                Confirmar
+              </Button>
+            </Link>
+            <Link to="/user/fornecedor">
+              <Button className="mb-4 float-left" color="danger">
+                Cancelar
+              </Button>
+            </Link>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = () => ({});
+const mapDispatchToProps = {};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddFornecedor);
---
+++ src/pages/estoquista/addProduto.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+
+import '../../styles/pages/login.scss';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, FormGroup, Label, Input, Card, CardBody, CardHeader, Button, Col, Row } from 'reactstrap';
+
+interface IAddProdutoProps extends StateProps, DispatchProps {}
+
+class AddProduto extends React.Component<IAddProdutoProps> {
+  render() {
+    return (
+      <div className="d-flex h-150 align-items-center justify-content-center">
+        <Card className="w-50 shadow-lg">
+          <CardHeader className="bg-dark text-white">Adicionar produto</CardHeader>
+          <CardBody>
+            <Form>
+              <Row form>
+                <Col md="6">
+                  <FormGroup row>
+                    <Label for="exampleSelect">Produto</Label>
+                    <Input type="select" name="select" id="exampleSelect">
+                      <option>Arroz</option>
+                      <option>Feijão</option>
+                    </Input>
+                  </FormGroup>
+                </Col>
+                <br></br>
+                <Col md="4">
+                  <FormGroup>
+                    <Label>Quantidade em estoque</Label>
+                    <Input placeholder="Autopreencher"></Input>
+                  </FormGroup>
+                </Col>
+              </Row>
+              <br></br>
+              <FormGroup>
+                <Label>Nome do produto</Label>
+                <Input placeholder="Digite aqui..."></Input>
+              </FormGroup>
+              <br></br>
+              <FormGroup>
+                <Label for="exampleSelect">Setor de detino</Label>
+                <Input type="select" name="select" id="exampleSelect">
+                  <option>Cozinha</option>
+                  <option>Panificação</option>
+                  <option>Limpeza</option>
+                  <option>Casa carinho</option>
+                  <option>Bezerra de menezes</option>
+                </Input>
+              </FormGroup>
+              <br></br>
+              <FormGroup>
+                <Label for="exapleNumber">Quantidade de KG que deseja tranferir</Label>
+                <Input type="number" name="numbe" id="exampleNumber"></Input>
+              </FormGroup>
+              <br></br>
+              <FormGroup>
+                <Label for="exampleText">Motivo da tranferência</Label>
+                <Input type="textarea" name="text" id="exampletest"></Input>
+              </FormGroup>
+            </Form>
+            <Link to="/user/estoque">
+              <Button className="mb-4 float-right" color="success">
+                Confirmar
+              </Button>
+            </Link>
+            <Link to="/user/estoque">
+              <Button className="mb-4 float-left" color="danger">
+                Cancelar
+              </Button>
+            </Link>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = () => ({});
+const mapDispatchToProps = {};
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddProduto);
